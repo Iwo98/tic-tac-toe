c94bb2fa7cf11e985601a6927056a1fa32b25e9a
Iwo <iwo.brzyski98@gmail.com>
2020-07-13 10:44:06 +0200

    Clear code with eslint's check, Working animations

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,63 +1,71 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import styles from './Board.module.scss';
-import Square from "../Square/Square";
+import Square from '../Square/Square';
 
-const Board = ({win, passWin, currentPlayer, passCurrentPlayer, passWinLine}) => {
 
-    const [valuesArray, setValuesArray] = useState(["", "", "", "", "", "", "", "", "",])
+const Board = ({
+  win, passWin, currentPlayer, passCurrentPlayer, passWinLine, 
+}) => {
+  const [valuesArray, setValuesArray] = useState(['', '', '', '', '', '', '', '', '']);
 
-    const playFn = (indexOfBox) => {
+  const winCheck = (figure, arrayOfValues) => {
+    const winLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-        const newState = [...valuesArray];
-        newState[indexOfBox] = currentPlayer;
-
-        winCheck(currentPlayer, newState);
-        setValuesArray(newState);
-
-        passCurrentPlayer(prevState => prevState === "X" ? "O" : "X");
+    for (let i = 0; i < winLines.length; i += 1) {
+      if ((arrayOfValues[winLines[i][0]] === figure)
+                && (arrayOfValues[winLines[i][1]] === figure)
+                && (arrayOfValues[winLines[i][2]] === figure)
+      ) {
+        passWin(true);
+        passWinLine(i);
+        break;
+      }
     }
+  };
 
-    const winFn = () => {
-        console.log("Juz przegrałes")
-    }
+    
+  const playFn = (indexOfBox) => {
+    const newState = [...valuesArray];
+    newState[indexOfBox] = currentPlayer;
 
-    const winCheck = (figure, arrayOfValues) => {
+    winCheck(currentPlayer, newState);
+    setValuesArray(newState);
 
-        const winLines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ]
+    passCurrentPlayer(prevState => (prevState === 'X' ? 'O' : 'X'));
+  };
 
-        for (let i = 0; i < winLines.length; i++) {
-            if ((arrayOfValues[winLines[i][0]] === figure) &&
-                (arrayOfValues[winLines[i][1]] === figure) &&
-                (arrayOfValues[winLines[i][2]] === figure)
-            ) {
-                passWin(true);
-                passWinLine(i)
-                break;
-            }
-        }
-    }
+  return (
+    <div className={styles.board}>
+      {valuesArray.map((item, indexOfItem) => (
+        <Square
+          onClickFn={() => {
+            playFn(indexOfItem);
+          }}
+          value={item}
+          win={win}
+        />
+      ))}
+    </div>
+  );
+};
 
-    return (
-            <div className={styles.board}>
-                {valuesArray.map((item, indexOfItem) =>
-                    <Square
-                        onClick={() => {
-                            win !== true ? playFn(indexOfItem) : winFn()
-                        }}
-                        value={item}
-                    />
-                )}
-            </div>
-    )
-}
+Board.propTypes = {
+  win: PropTypes.bool.isRequired,
+  passWin: PropTypes.func.isRequired,
+  passWinLine: PropTypes.func.isRequired,
+  currentPlayer: PropTypes.string.isRequired,
+  passCurrentPlayer: PropTypes.func.isRequired,
+};
 
 export default Board;
+
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Square.module.scss';
 
 
-const Square = ({value="",onClick,disable}) => {
-    return (
-            <button
-                className={styles.square}
-                type="button"
-                onClick={onClick}
-                disabled={value !== "" ? true : false }
-              >
-                {value}
-            </button>
-    )
+const Square = ({ value, onClickFn, win }) => (
+  <button
+    className={styles.square}
+    type="button"
+    onClick={onClickFn}
+    disabled={value !== '' || win === true}
+  >
+    {value}
+  </button>
+);
+
+Square.propTypes = {
+  value: PropTypes.string,
+  onClickFn: PropTypes.func.isRequired,
+  win: PropTypes.bool.isRequired,
+};
+
+Square.defaultProps = {
+  value: '',
 };
 
 export default Square;
